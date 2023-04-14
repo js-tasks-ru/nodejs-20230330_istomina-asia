744d8a1238e10b0217be967e089fc5ec15681232
Ase444ka <istomina.asia@yandex.ru>
2023-04-14 10:36:19 +0300

    sum: format file

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,6 +1,6 @@
 function sum(a, b) {
-  if (typeof a !== "number" || typeof b !== "number") {
-    throw new TypeError("Оба параметра должны быть числовыми!");
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new TypeError('Оба параметра должны быть числовыми!');
   }
   return a + b;
 }
